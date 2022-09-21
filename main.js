2fb5cf83de39e3c1b53988523cedcc0c2d539a7e
Anita9948 <anitabaljit92@gmail.com>
2022-09-21 17:38:26 -0400

    make correct letter  visible

--- main.js
+++ main.js
@@ -41,34 +41,55 @@ const randomWord = [
 ];
 
 const displayMessage = "Try Again";
+const points = 5;
 
 const genNum = () => Math.floor(Math.random() * randomWord.length);
 const answer = randomWord[genNum()];
 
 function render() {
   for (let i = 0; i < acbLetters.length; i++) {
-    const pickedLetters = document.createElement("div");
-    pickedLetters.innerText = acbLetters[i];
-    document.getElementById("main-letters").append(pickedLetters);
+    const mainKeyBoard = document.createElement("div");
+    mainKeyBoard.innerText = acbLetters[i];
+    document.getElementById("main-letters").append(mainKeyBoard);
   }
 }
 render();
 
 function createSpan() {
-  for (let j = 0; j < answer.length; j++) {
+  for (let i = 0; i < answer.length; i++) {
     const span = document.createElement("span");
-    span.innerText = answer[j];
+    span.innerText = answer[i];
     document.querySelector(".word").append(span);
   }
 }
 createSpan();
 
 function handleClick(e) {
-  console.log(e.target);
+  curDiv = e.target;
+  const spanEls = document.querySelectorAll("span");
+  if (isLetterInWord(curDiv)) {
+    showMatchingSpans(spanEls,curDiv)
+  }
+  e.target.style.color = isLetterInWord(curDiv) ? 'green' : 'red'
 }
 
 document.getElementById("main-letters").addEventListener("click", handleClick);
 
-
-
-
+function isLetterInWord(cl) {
+  let result = false;
+  for (let i = 0; i < answer.length; i++) {
+    if (cl.textContent === answer[i].toUpperCase()) {
+      result = true;
+    }
+  }
+  return result;
+}
+function showMatchingSpans(spans,cl) {
+    for (let i = 0; i < spans.length; i++) {
+        let currentSpanEls = spans[i];
+        console.log(i);
+        if (currentSpanEls.textContent.toUpperCase() === cl.textContent) {
+          currentSpanEls.style.visibility = "visible";
+        }
+      }
+}
