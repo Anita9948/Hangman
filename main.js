6a0e55cdd8b93c321f2e309c289de636412c128b
Anita9948 <anitabaljit92@gmail.com>
2022-09-21 18:47:52 -0400

    mainkeyboard turns red or green

--- main.js
+++ main.js
@@ -40,20 +40,25 @@ const randomWord = [
   "films",
 ];
 
-const displayMessage = "Try Again";
-const points = 5;
+let totalNumberGuesses = 6;
 
 const genNum = () => Math.floor(Math.random() * randomWord.length);
 const answer = randomWord[genNum()];
+init();
+
+function init() {
+  createDiv();
+  createSpan();
+  document
+    .getElementById("main-letters")
+    .addEventListener("click", handleClick);
+}
 
 function render() {
-  for (let i = 0; i < acbLetters.length; i++) {
-    const mainKeyBoard = document.createElement("div");
-    mainKeyBoard.innerText = acbLetters[i];
-    document.getElementById("main-letters").append(mainKeyBoard);
-  }
+  document.getElementById(
+    "incorrect"
+  ).innerText = `Remaining Guesses: ${totalNumberGuesses}`;
 }
-render();
 
 function createSpan() {
   for (let i = 0; i < answer.length; i++) {
@@ -62,18 +67,6 @@ function createSpan() {
     document.querySelector(".word").append(span);
   }
 }
-createSpan();
-
-function handleClick(e) {
-  curDiv = e.target;
-  const spanEls = document.querySelectorAll("span");
-  if (isLetterInWord(curDiv)) {
-    showMatchingSpans(spanEls,curDiv)
-  }
-  e.target.style.color = isLetterInWord(curDiv) ? 'green' : 'red'
-}
-
-document.getElementById("main-letters").addEventListener("click", handleClick);
 
 function isLetterInWord(cl) {
   let result = false;
@@ -84,12 +77,36 @@ function isLetterInWord(cl) {
   }
   return result;
 }
-function showMatchingSpans(spans,cl) {
-    for (let i = 0; i < spans.length; i++) {
-        let currentSpanEls = spans[i];
-        console.log(i);
-        if (currentSpanEls.textContent.toUpperCase() === cl.textContent) {
-          currentSpanEls.style.visibility = "visible";
-        }
-      }
+function showMatchingSpans(spans, cl) {
+  for (let i = 0; i < spans.length; i++) {
+    let currentSpanEls = spans[i];
+    if (currentSpanEls.textContent.toUpperCase() === cl.textContent) {
+      currentSpanEls.style.visibility = "visible";
+    }
+  }
 }
+
+function handleClick(e) {
+  let curDiv = e.target;
+  if (curDiv.style.color === "red" || curDiv.style.color === "green") return;
+  const spanEls = document.querySelectorAll("span");
+  if (isLetterInWord(curDiv)) {
+    showMatchingSpans(spanEls, curDiv);
+  }
+  e.target.style.color = isLetterInWord(curDiv) ? "green" : "red";
+  totalNumberGuesses--;
+  render();
+}
+
+function createDiv() {
+  for (let i = 0; i < acbLetters.length; i++) {
+    const mainKeyBoard = document.createElement("div");
+    mainKeyBoard.innerText = acbLetters[i];
+    document.getElementById("main-letters").append(mainKeyBoard);
+  }
+}
+
+let winner = document.querySelectorAll("span");
+if (winner >= 0 && winner === "visible" ) {
+    
+} 
